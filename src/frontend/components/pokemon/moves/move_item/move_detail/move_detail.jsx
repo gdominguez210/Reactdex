931fc669369784139ef98152cbaececeb136bb7c
Gary <gdominguez210@live.com>
2020-02-23 23:30:26 -0500

    add rough layout for move details

--- src/frontend/components/pokemon/moves/move_item/move_detail/move_detail.jsx
+++ src/frontend/components/pokemon/moves/move_item/move_detail/move_detail.jsx
@@ -8,6 +8,16 @@ export const MoveDetails = (props) => {
     const move = moves[name];
     console.log(move);
     return (
-        <div>Placeholder</div>
+        <div>
+            <h2>{move.name}</h2>
+            <ul>
+                <li>Type: {move.type.name}</li>
+                <li>Damage Type: {move.damage_class.name}</li>
+                <li>Power: {move.power}</li>
+                <li>PP: {move.pp}</li>
+                <li>Accuracy: {move.accuracy}</li>
+            </ul>
+            <p>{move.effect_entries[0].effect}</p>
+        </div>
     )
 }
